6383b119e898380e9ab55e84b3d54b757a37d91d
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-06-15 08:00:06 -0700

    data we need to track

--- src/App.js
+++ src/App.js
@@ -11,4 +11,20 @@ function App() {
 export default App
 
 /* 
+data we need to track:
+  -- solution
+    -- 5 letter string, e.g. 'drain'
+  -- past guesses
+    -- an array of past guesses
+    -- each past guess is an array of letter objects [{}, {}, {}, {}, {}]
+    -- each object represents a letter in the guess word {letter: 'a', color: 'yellow'}
+  -- current guess
+    -- string 'hello'
+  -- keypad letters
+    -- array of letter objects [{key: 'a', color: 'green'}, {}, {} ...]
+  -- number of turns
+    -- an integer 0 - 6
 
+
+
+*/
