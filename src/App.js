21c71ede0529442d7de871aeed402ddcf89ace40
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-06-15 08:02:38 -0700

    cheking submitted words

--- src/App.js
+++ src/App.js
@@ -32,6 +32,15 @@ game process:
     -- when a user hits enter it submits the word
       -- if all squares are not filled with letters then the word is not submitted
       -- if that word has already been used in a prev guess then the word is not submitted
+  -- checking submitted words:
+    -- each letter is checked to see if it matches to the solution
+    -- each letter is assigned a color based on it's inclusion in the solution
+      -- exact matches (correct position in the solution) are green
+      -- partial matches (in the solution but not the correct position) are yellow
+      -- none-matches (not in the solution at all) are grey
+    -- the guess is added the grid with the correct colors
+    -- the current guess moves to the next row
+    -- the keypad letters are updated (colors)
 
 
 */
