a0fbd0ccb28c5f42fb59aaf26c23417f3e540d23
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-06-15 08:36:43 -0700

    read a random ward from db

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [solution, setSolution] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:3003/solutions")
+      .then(res => res.json())
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+
+      
+      });
+  }, [setSolution]);
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>solution is : {solution}</div>}
     </div>
   );
 }
